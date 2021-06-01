a34221ca0798c63a6d5f4ac74cb0a6209a354556
rtimelston <13246079+rtimelston@users.noreply.github.com>
2021-05-31 18:51:16 -0700

    Section 3.24 Streaming Events

--- src/main/java/guru/springframework/netflux/controllers/MovieController.java
+++ src/main/java/guru/springframework/netflux/controllers/MovieController.java
@@ -1,8 +1,10 @@
 package guru.springframework.netflux.controllers;
 
 import guru.springframework.netflux.domain.Movie;
+import guru.springframework.netflux.domain.MovieEvent;
 import guru.springframework.netflux.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,10 @@ public class MovieController {
     @GetMapping("")
     Flux<Movie> getAllMovies() {
         return movieService.getAllMovies();
+    }
+
+    @GetMapping(path = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id) {
+        return movieService.streamMovieEvents(id);
     }
 }
